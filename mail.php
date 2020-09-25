528a1a94059fb9627dae0cceadd83e2469d6ae45
Imoh Henry <henryimoh2@gmail.com>
2020-09-25 05:22:39 +0100

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,42 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__ . '/PHPMailer-master/src/Exception.php';
+require_once __DIR__ . '/PHPMailer-master/src/PHPMailer.php';
+require_once __DIR__ . '/PHPMailer-master/src/SMTP.php';
+
+// passing true in constructor enables exceptions in PHPMailer
+$mail = new PHPMailer(true);
+
+try {
+    // Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; // for detailed debug output
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->Username = 'Henryimoh2@gmail.com'; // YOUR gmail email
+    $mail->Password = 'sharon9358'; // YOUR gmail password
+
+    // Sender and recipient settings
+    $mail->setFrom('example@gmail.com', 'Sender Name');
+    $mail->addAddress('phppot@example.com', 'Receiver Name');
+    $mail->addReplyTo('example@gmail.com', 'Sender Name'); // to set the reply to
+
+    // Setting the email content
+    $mail->IsHTML(true);
+    $mail->Subject = "Send email using Gmail SMTP and PHPMailer";
+    $mail->Body = 'HTML message body. <b>Gmail</b> SMTP email body.';
+    $mail->AltBody = 'Plain text message body for non-HTML email client. Gmail SMTP email body.';
+
+    $mail->send();
+    echo "Email message sent.";
+} catch (Exception $e) {
+    echo "Error in sending email. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
