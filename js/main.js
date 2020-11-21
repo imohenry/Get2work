54671aa3e41bed37bcdc372d2d9945352ac21739
Imoh Henry <henryimoh2@gmail.com>
2020-11-21 07:14:28 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -202,7 +202,7 @@
       animation: 'fade', // Fade, slide, none
       animationInSpeed: 200, // Animation in speed (ms)
       animationOutSpeed: 200, // Animation out speed (ms)
-      scrollText: '<i class="ti-arrow-up"></i>', // Text for element
+      scrollText: '<i class="flaticon-up-arrow"></i>', // Text for element
       activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
     });
 
